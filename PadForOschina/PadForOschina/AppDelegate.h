f89693b03359dd44d4a49eee99d54d9b209ec3dd
buffaloboyhu <buffaloboyhu@163.com>
2016-07-05 22:51:42 +0800

    创建PadForOschina工程

---
+++ PadForOschina/PadForOschina/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PadForOschina
+//
+//  Created by Buffalo on 16/7/5.
+//  Copyright © 2016年 BBH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
